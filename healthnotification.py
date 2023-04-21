d7cf87cb7124629a41078143928920cae1062a9f
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-04-21 22:19:01 +0530

    Update healthnotification.py

--- healthnotification.py
+++ healthnotification.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
 account_sid = "AC43ec2a5124b04a86b9f13c4c55f6aa03"
-auth_token = "1c24139daf9aeb4f5760251ea8ad9b12"
+auth_token = "fbfbbfceb16eda3d10f53d020aa9546b"
 client = Client(account_sid, auth_token)
 message = client.messages.create(
   body="Hello from Twilio",
