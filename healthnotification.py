a81310112eaca9ee6745db661ef7a7bf5b071f23
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-04-27 20:55:01 +0530

    Update healthnotification.py

--- healthnotification.py
+++ healthnotification.py
@@ -6,7 +6,7 @@ account_sid = "AC43ec2a5124b04a86b9f13c4c55f6aa03"
 auth_token = "1e6fe68db284b1217f5e735bcd701f5d"
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-  body="Hello from Family Medical.Feel free to ask if you have any queries",
+  body="Hello from Family Medical.Feel free to ask if you have any queries.or else let us have a meet through this https://tanishqravula.github.io/tanishq/REMO/video.html",
   from_="+12765229583",
   to="+917702337176"
 )
