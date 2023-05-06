130e62d1798237566a575bcda8001c442548b7a8
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-05-06 21:05:08 +0530

    Update visualise.py

--- Stress-Level-Detector/Tabs/visualise.py
+++ Stress-Level-Detector/Tabs/visualise.py
@@ -11,6 +11,7 @@ import streamlit as st
 
 # Import necessary functions from web_functions
 from web_functions import train_model
+from main import app
 
 def app(df, X, y):
     """This function create the visualisation page"""
