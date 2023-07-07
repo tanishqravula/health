eb72fab2ff7b02b10ce48824b78460cd7ffa807c
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-07-07 19:52:25 +0530

    Update about.py

--- Diabetes-Prediction-System/Tabs/about.py
+++ Diabetes-Prediction-System/Tabs/about.py
@@ -9,9 +9,4 @@ def app():
     """This function creates the about page"""
     st.balloons()
     st.title('Contact Us')
-    st.markdown('''### Name: Mainak Chaudhuri''')
-    st.markdown('''Passionate Engineer and Rational Thinker. Data Scientist, Web Developer, Blockchain Maestro, Quantum Computing Tutor''')
-    st.image('./images/icon.jpg')
-    st.markdown('''### Linkedin: [Mainak](https://www.linkedin.com/in/mainak-chaudhuri-127898176/)''')
-    st.markdown('''### GitHub: [Mainak](https://github.com/MainakRepositor/)''')
     
