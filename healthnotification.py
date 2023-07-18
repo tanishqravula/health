7d92036bd5c4a3a64bcfd6a9d565c66a228942a6
tanishqravula <121391761+tanishqravula@users.noreply.github.com>
2023-07-18 14:37:42 +0530

    Update healthnotification.py

--- healthnotification.py
+++ healthnotification.py
@@ -2,12 +2,12 @@ import os
 from twilio.rest import Client
 # Set environment variables for your credentials
 # Read more at http://twil.io/secure
-account_sid = "AC43ec2a5124b04a86b9f13c4c55f6aa03"
-auth_token = "1e6fe68db284b1217f5e735bcd701f5d"
+account_sid = "AC57b15b2f874c753f76cd667a0712be0b"
+auth_token = "384a232180244dcf6cf890eec81e894e"
 client = Client(account_sid, auth_token)
 message = client.messages.create(
-  body="Hello from Family Medical.Feel free to ask if you have any queries.or else let us have a meet through this https://tanishqravula.github.io/tanishq/REMO/video.html and send meeting link to us through tanishqravula@gmail.com",
-  from_="+12765229583",
+   body="Hello from Family Medical.Feel free to ask if you have any queries.or else let us have a meet through this https://tanishqravula.github.io/tanishq/REMO/video.html and send meeting link to us through tanishqravula@gmail.com",
+  from_="+19897109806",
   to="+917702337176"
 )
 print(message.sid)
